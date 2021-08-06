5ce5b5d88e16f32ae6a1dc7b1f90ed7a9bcfab5e
skusVV <skus1992@gmail.com>
2021-08-06 23:54:57 +0300

    setting up infrastructure and auth service

---
+++ auth/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+const app = express();
+
+app.use(json());
+
+app.get('/api/users/current-user', (req, res) => {
+    return res.send('Hi there');
+});
+
+
+app.listen(3000, () => {
+    console.log('Auth listen on port 3000');
+});
