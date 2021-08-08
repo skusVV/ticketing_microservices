8cd61cc8eef6fdf895eef49beed71b5a206a3329
skusVV <skus1992@gmail.com>
2021-08-08 22:26:37 +0300

    connected mongoDB

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import  'express-async-errors'
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { CurrentUserRouter } from './routes/current-user';
 import { SignInUserRouter } from './routes/sign-in';
 import { SignOutUserRouter } from './routes/sign-out';
@@ -22,6 +24,24 @@ app.all('/*', async() => {
 
 app.use(requestHandler);
 
-app.listen(3000, () => {
-    console.log('Auth listen on port 3000');
-});
+const init = async() => {
+    try {
+        await mongoose.connect( 'mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('Connected');
+    } catch (e) {
+        console.log(e);
+    }
+
+    app.listen(3000, () => {
+        console.log('Auth listen on port 3000');
+    });
+};
+
+init();
+
+
+
